b5a5727c036e5ec6f2f232e4c1712a30fd5f1063
Nilesh <pnilesh120897@gmail.com>
2019-02-23 20:42:59 +0530

    multidex added

--- app/src/main/java/com/daima/trycatch/daima/SignUpMobile.java
+++ app/src/main/java/com/daima/trycatch/daima/SignUpMobile.java
@@ -2,12 +2,32 @@ package com.daima.trycatch.daima;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class SignUpMobile extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_mobile);
+
+
+
+
     }
 }
