dedb99448d8df9e7cd6e58e68836dd0fa31759b0
Naman <namansandhya@gmail.com>
2019-02-23 11:57:53 +0530

    Push Notifications

--- app/src/main/java/com/daima/trycatch/daima/MainActivity.java
+++ app/src/main/java/com/daima/trycatch/daima/MainActivity.java
@@ -1,10 +1,12 @@
 package com.daima.trycatch.daima;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,5 +42,6 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 
 }
